4742363209f2f9c2cb8d357da7f1b7d76bef122a
Anton Novikov <tonn.post@gmail.com>
2021-03-15 15:18:17 +0400

    feat: implemented contexts list

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { bind } from 'lodash-decorators';
 import React from 'react';
 import './App.scss';
 import { BEM } from './BEM';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import fontawesome from '@fortawesome/fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import _ from 'lodash';
 
-/*
-  TODO: 
-  1. save state to localstorage
-  2. appearence
-  3. contexts
-*/
+fontawesome.library.add(faTrash as any);
+
+// TODO: 1. save state to localstorage
+// TODO: 2. appearence
+// TODO: 3. contexts
+// TODO: 4. recommendation to rest and work
+// TODO: 5. settings
+// TODO: 6. pwa-update button
+
 
 type TimerStateType = 'focusing' | 'stopped';
 
@@ -34,9 +41,12 @@ function getStartAndEndOfDay(day: Date): { start: Date, end: Date } {
 }
 
 const DayTicksLength = 24 * 60 * 60 * 1000;
+const LSIntervals = 'LSIntervals';
+const LSContexts = 'LSContexts';
+const Colors = ['blue', 'red', 'green', 'yellow', 'black', 'orange'];
 
 export class IntervalsTimeline extends React.Component<
-  { Intervals: ITimeInterval[] }, 
+  { Intervals: ITimeInterval[] },
   { DaysOffset: number, DaysCount: number }> {
 
   private _hours = [...Array(24).keys()].map(h => h + 1);
@@ -56,7 +66,7 @@ export class IntervalsTimeline extends React.Component<
       .map(d => {
         const day = new Date();
         day.setDate(day.getDate() - d);
-        
+
         const dayInterval = getStartAndEndOfDay(day);
 
         return Intervals
@@ -64,7 +74,7 @@ export class IntervalsTimeline extends React.Component<
           .map(interval => {
             const start = interval.start.getTime() - dayInterval.start.getTime();
             const width = interval.stop!.getTime() - interval.start.getTime();
-            
+
             return { interval, left: 100 * start / DayTicksLength, width: 100 * width / DayTicksLength };
           });
       });
@@ -82,7 +92,7 @@ export class IntervalsTimeline extends React.Component<
             <div className={elem('Day')}>
               {day.map(ivm => (
                 <div className={elem('Interval')} style={{ left: `${ivm.left}%`, width: `${ivm.width}%` }}>
-                </div> 
+                </div>
               ))}
             </div>
           ))
@@ -94,17 +104,27 @@ export class IntervalsTimeline extends React.Component<
 
 const { block, elem } = BEM('IntervalsTimeline');
 
+interface IContext { name: string, color: string, current: boolean, readonly?: true }
+
 export class App extends React.Component<{}, {}> {
   private timerState: TimerStateType = 'stopped';
   private intervalRef: NodeJS.Timeout | undefined;
   private timeIntervals: ITimeInterval[] = [];
   private currentTimeInterval: ITimeInterval | undefined;
+  private contexts: IContext[] = [{ name: 'default', color: 'lightgray', current: true, readonly: true }];
+
 
   private config = {
     pomodoroLength: 25,
     restLength: 5,
     longRestPerMinutes: 120,
     longRestLength: 60
+  }
+
+  constructor(props: any) {
+    super(props);
+
+    this.restore();
   }
 
   @bind
@@ -139,6 +159,66 @@ export class App extends React.Component<{}, {}> {
   @bind
   private startStopButtonClick() {
     this.setTimerState(this.timerState === 'focusing' ? 'stopped' : 'focusing');
+
+    this.save();
+  }
+
+  private save() {
+    localStorage[LSIntervals] = JSON.stringify(this.timeIntervals);
+    localStorage[LSContexts] = JSON.stringify(this.contexts);
+  }
+
+  private restore() {
+    if (localStorage[LSIntervals]) {
+      this.timeIntervals = JSON.parse(localStorage[LSIntervals]);
+
+      this.timeIntervals.forEach(i => {
+        i.start = new Date(i.start);
+        i.stop = i.stop && new Date(i.stop);
+      });
+    }
+
+    if (localStorage[LSContexts]) {
+      this.contexts = JSON.parse(localStorage[LSContexts]);
+    }
+  }
+
+  private getUnusedColor() {
+    return Colors.find(color => !this.contexts.some(ctx => ctx.color === color)) || 'black';
+  }
+
+  @bind
+  private addContext() {
+    this.contexts.push({ name: '', color: this.getUnusedColor(), current: false });
+
+    this.save();
+    this.setState({});
+  }
+
+  private contextNameChanged(e: React.ChangeEvent<HTMLInputElement>, context: IContext) {
+    context.name = e.currentTarget.value;
+
+    this.save();
+    this.setState({});
+  }
+
+  private removeContext(e: React.MouseEvent, context: IContext) {
+    e.stopPropagation();
+
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm(`Remove "${context.name}"?`)) {
+      _.remove(this.contexts, context);
+    }
+
+    this.save();
+    this.setState({});
+  }
+
+  private contextClicked(context: IContext) {
+    this.contexts.forEach(ctx => ctx.current = (ctx === context));
+
+    this.save();
+    this.setState({});
   }
 
   render() {
@@ -150,6 +230,22 @@ export class App extends React.Component<{}, {}> {
           { timerState === 'stopped' ? 'Focus!' : 'Stop' }
         </div>
         <IntervalsTimeline Intervals={this.timeIntervals} />
+        <div className={appElem('Contexts')}>
+          {
+            this.contexts.map(context => (
+              <div className={appElem('Context', context.current ? 'Current' : undefined)} onClick={() => this.contextClicked(context)}>
+                <div className={appElem('ContextColor')} style={{ backgroundColor: context.color }} />
+
+                <input value={context.name} onChange={e => this.contextNameChanged(e, context)} readOnly={context.readonly}/>
+
+                { context.readonly ? null :
+                  <div className={appElem('ContextRemove')} onClick={e => this.removeContext(e, context)}>
+                    <FontAwesomeIcon icon='trash' />
+                  </div> }
+              </div>))
+          }
+          <div className={appElem('AddContextButton')} onClick={this.addContext}>Add</div>
+        </div>
       </div>
     );
   }
