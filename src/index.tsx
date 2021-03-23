6e49ab12d52e73e9fbf3556120abca595ebb97cb
Anton Novikov <tonn.post@gmail.com>
2021-03-23 14:40:18 +0400

    refact

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import { App } from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.body
 );
 
 // If you want your app to work offline and load faster, you can change
